0c98193fcb3799ca93ff5338a8b2b9168ed48835
Joel Mendoza <wester.mendoza@epn.edu.ec>
2020-07-07 16:30:35 -0500

    #16 Relaciones en Eloquent

--- app/Comment.php
+++ app/Comment.php
@@ -8,4 +8,11 @@ class Comment extends Model
 {
     //
     protected $fillable =['text'];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+    public function article(){
+        return $this->belongsTo('App\Article');
+    }
 }
