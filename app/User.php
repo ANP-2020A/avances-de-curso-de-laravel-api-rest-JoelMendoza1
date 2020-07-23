2b3810f6de50a8193a1befddbbf4ff3d2b9b2aa6
Joel Mendoza <wester.mendoza@epn.edu.ec>
2020-07-23 16:03:48 -0500

    #18 Relaciones

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable implements JWTSubject
 {
@@ -49,5 +50,8 @@ class User extends Authenticatable implements JWTSubject
     }
     public function comments(){
         return $this->hasMany('App\Comment');
+    }
+    public function categories(){
+        return $this->belongsToMany('App\Category')->as('subscriptions')->withTimestamps();
     }
 }
